5e1f802db0bdc42be78d979e51336f85151c8ddb
Antonio Filho <antoniofilhocastro001@gmail.com>
2023-06-19 05:58:28 -0300

    Create Class Position

---
+++ src/boardgame/Position.java
@@ -0,0 +1,36 @@
+package boardgame;
+
+public class Position {
+	private String row;
+	private int column;
+	
+	public Position(String row, int column) {
+		super();
+		this.row = row;
+		this.column = column;
+	}
+
+	public String getRow() {
+		return row;
+	}
+
+	public void setRow(String row) {
+		this.row = row;
+	}
+
+	public int getColumn() {
+		return column;
+	}
+
+	public void setColumn(int column) {
+		this.column = column;
+	}
+
+	@Override
+	public String toString() {
+		return row + ", " + column;
+	}
+	
+	
+	
+}
