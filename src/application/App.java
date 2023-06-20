e7b6a5902910c368b40b12c82a4692d25ee4bd8e
Antonio Filho <antoniofilhocastro001@gmail.com>
2023-06-20 19:41:06 -0300

    Check and CheckMatch

--- src/application/App.java
+++ src/application/App.java
@@ -16,7 +16,7 @@ public class App {
 		ChessMatch chessMatch = new ChessMatch();
 		List<ChessPiece> captured = new ArrayList<>();
 	 
-		while (true) {
+		while (!chessMatch.getCheckMate()) {
 			try {
 				UI.clearScreen();
 				UI.printBoard(chessMatch.getPiece());
--- src/application/UI.java
+++ src/application/UI.java
@@ -44,10 +44,19 @@ public class UI {
 			printCapturedPieces(captured);
 			System.out.println();
 			System.out.println("Turn : " + chessMatch.getTurn());
-			System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+			if (!chessMatch.getCheckMate()) {
+				System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+				if (chessMatch.getCheck()) {
+					System.out.println("CHECK!");
+				}
+			}
+			else {
+				System.out.println("CHECKMATE!");
+				System.out.println("Winner: " + chessMatch.getCurrentPlayer());
+			}
 		}
 		
-	public static ChessPosition readChessPositon(Scanner sc) {
+		public static ChessPosition readChessPositon(Scanner sc) {
 		try {
 			String s = sc.nextLine();
 			char column = s.charAt((0));
